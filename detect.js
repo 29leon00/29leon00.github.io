1e030b6df1ae855104f5ee8a634427dde6b6d4ef
29leon00 <125478675+29leon00@users.noreply.github.com>
2024-08-24 20:22:12 +0200

    Update detect.js

--- detect.js
+++ detect.js
@@ -1,104 +1,35 @@
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.videoRef = React.createRef();
-    this.canvasRef = React.createRef();
-    this.state = {
-      currentStream: null,
-      model: null,
-      cameras: [],
-      currentCameraIndex: 0,
-    };
+     const loadlModelPromise = cocoSsd.load();
+      
+      // resolve all the Promises
+      Promise.all([loadlModelPromise, webcamPromise])
+        .then(values => {
+          this.detectFromVideoFrame(values[0], this.videoRef.current);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
   }
 
-  async componentDidMount() {
-    const model = await cocoSsd.load();
-    this.setState({ model });
-    await this.initCamera();
-  }
-
-  initCamera = async () => {
-    try {
-      const devices = await navigator.mediaDevices.enumerateDevices();
-      const videoDevices = devices.filter(device => device.kind === 'videoinput');
-      this.setState({ cameras: videoDevices });
-
-      if (videoDevices.length > 0) {
-        this.switchCamera();
-      } else {
-        alert('Aucune caméra détectée');
-      }
-    } catch (error) {
-      console.error("Erreur lors de l'accès à la caméra:", error);
-    }
-  };
-
-  switchCamera = async () => {
-    const { cameras, currentCameraIndex, currentStream } = this.state;
-    if (currentStream) {
-      currentStream.getTracks().forEach(track => track.stop());
-    }
-
-    const nextCameraIndex = (currentCameraIndex + 1) % cameras.length;
-    const selectedCamera = cameras[nextCameraIndex];
-
-    const stream = await navigator.mediaDevices.getUserMedia({
-      video: { deviceId: selectedCamera.deviceId },
-    });
-
-    this.videoRef.current.srcObject = stream;
-    this.setState({ currentStream: stream, currentCameraIndex: nextCameraIndex });
-
-    this.detectFromVideoFrame(this.state.model, this.videoRef.current);
-  };
-
-  detectFromVideoFrame = (model, video) => {
-    const detect = async () => {
-      const predictions = await model.detect(video);
-      this.renderPredictions(predictions);
-
-      requestAnimationFrame(detect);
-    };
-    detect();
-  };
-
-  renderPredictions = (predictions) => {
-    const ctx = this.canvasRef.current.getContext("2d");
-    ctx.clearRect(0, 0, this.canvasRef.current.width, this.canvasRef.current.height);
-    ctx.drawImage(this.videoRef.current, 0, 0, this.canvasRef.current.width, this.canvasRef.current.height);
-
-    predictions.forEach(prediction => {
-      ctx.beginPath();
-      ctx.rect(...prediction.bbox);
-      ctx.lineWidth = 2;
-      ctx.strokeStyle = "green";
-      ctx.fillStyle = "green";
-      ctx.stroke();
-      ctx.fillText(
-        prediction.class + " : " + Math.round(prediction.score * 100) + "%",
-        prediction.bbox[0],
-        prediction.bbox[1] > 10 ? prediction.bbox[1] - 5 : 10
-      );
-    });
-  };
-
+  // here we are returning the video frame and canvas to draw,
+  // so we are in someway drawing our video "on the go"
   render() {
     return (
-      <div>
+      <div> 
         <video
-          ref={this.videoRef}
-          style={{ display: "none" }}
+          style={this.styles}
           autoPlay
           muted
           playsInline
+          ref={this.videoRef}
           width="720"
           height="600"
         />
-        <canvas ref={this.canvasRef} width="720" height="600" />
+        <canvas style={this.styles} ref={this.canvasRef} width="720" height="650" />
       </div>
     );
   }
 }
 
 const domContainer = document.querySelector('#root');
-const appInstance = ReactDOM.render(React.createElement(App), domContainer);
+ReactDOM.render(React.createElement(App), domContainer);
